09cc69e23f49e0e15bbcb8fa9c2fe3c6de0d81e9
haazime <h4zime@gmail.com>
2014-09-06 21:39:43 +0900

    サイズ内で何も消えないテストケースを追加

--- spec/lru_cache_spec.rb
+++ spec/lru_cache_spec.rb
@@ -4,6 +4,26 @@ RSpec.describe LRUCache do
   context 'サイズが2の時' do
     let(:cache) { LRUCache.new(2) }
 
+    it '何も消えない' do
+      expect(cache.to_hash).to match({})
+    end
+
+    it '何も消えない' do
+      cache[:a] = 'alpha'
+      expect(cache.to_hash).to match(
+        a: 'alpha'
+      )
+    end
+
+    it '何も消えない' do
+      cache[:a] = 'alpha'
+      cache[:b] = 'bravo'
+      expect(cache.to_hash).to match(
+        a: 'alpha',
+        b: 'bravo',
+      )
+    end
+
     it ':aが消える' do
       cache[:a] = 'alpha'
       cache[:b] = 'bravo'
