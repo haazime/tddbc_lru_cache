ea281b49b78d1c567d4ef8c29a3624bb9fc9c340
haazime <h4zime@gmail.com>
2014-09-07 10:07:49 +0900

    @stored_key_orderを@delete_key_orderに変更

--- lib/lru_cache.rb
+++ lib/lru_cache.rb
@@ -3,20 +3,20 @@ class LRUCache
   def initialize(capacity)
     @capacity = capacity
     @container = {}
-    @stored_key_order = []
+    @delete_key_order = []
   end
 
   def []=(key, value)
     @container[key] = value
-    @stored_key_order << key
-    if @stored_key_order.size > @capacity
-      @container.delete(@stored_key_order.shift)
+    @delete_key_order << key
+    if @delete_key_order.size > @capacity
+      @container.delete(@delete_key_order.shift)
     end
   end
 
   def [](key)
-    @stored_key_order.delete(key)
-    @stored_key_order << key
+    @delete_key_order.delete(key)
+    @delete_key_order << key
     @container[key]
   end
 
